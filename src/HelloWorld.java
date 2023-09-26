4a9750d73d1b6157597af8260f5a047064954841
SameeK2 <sameek2@nycstudents.net>
2023-09-26 09:06:43 -0400

    added code for the miniproject

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,23 @@
+
+import java.util.Scanner;
 public class HelloWorld {
     public static void main(String[] args)
     {
-        System.out.print("Hello worl");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("What was the total bill cost? ");
+        double bill = scan.nextDouble();
+        System.out.print("What is the tip percentage? ");
+        int tipPercentage = scan.nextInt();
+        System.out.print("What is the number of people? ");
+        int peopleNumber = scan.nextInt();
+
+        double tipperPerson = ((tipPercentage/100) * bill) / peopleNumber;
+        double totalperPerson = (((tipPercentage/100) * bill) / peopleNumber) + (bill / peopleNumber);
+        System.out.print("Your tip per person value is: " + tipperPerson);
+        System.out.print("Your total per person value is: " + totalperPerson);
+
+
+
     }
 }
