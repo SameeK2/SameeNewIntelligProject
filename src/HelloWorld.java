1853ba5ca5a3aeeabf23eb2bfbbbc352221d8fcb
SameeK2 <sameek2@nycstudents.net>
2023-09-27 16:27:59 -0400

    finished the assignment

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -12,10 +12,14 @@ public class HelloWorld {
         System.out.print("What is the number of people? ");
         int peopleNumber = scan.nextInt();
 
-        double tipperPerson = ((tipPercentage/100) * bill) / peopleNumber;
-        double totalperPerson = (((tipPercentage/100) * bill) / peopleNumber) + (bill / peopleNumber);
+        double totalTip = ((tipPercentage/100.0) * bill);
+        double tipperPerson = ((tipPercentage/100.0) * bill) / peopleNumber;
+        double totalperPerson = (((tipPercentage/100.0) * bill) / peopleNumber) + (bill / peopleNumber);
+        double totalingeneral = totalperPerson * peopleNumber;
         System.out.println("Your tip per person value is: " + tipperPerson);
-        System.out.print("Your total per person value is: " + totalperPerson);
+        System.out.println("Your total per person value is: " + totalperPerson);
+        System.out.println("Your totaltip value is: " + totalTip);
+        System.out.print("Your total for all individuals is: " + totalingeneral);
 
 
 
